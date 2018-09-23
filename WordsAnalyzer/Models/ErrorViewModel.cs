ece8cac7065c1ea60e30c63d27a217018f3e8e9a
Olexiy Dmytrash <olexiy.dmytrash@gmail.com>
2018-09-23 14:23:38 +0300

    Add project files.

---
+++ WordsAnalyzer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WordsAnalyzer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
